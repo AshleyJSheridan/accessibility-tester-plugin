80111ad0059f6d90139b8b5e0cf501067c9ea50e
Ashley Sheridan <ashley.sheridan@livedrive.com>
2019-03-13 14:09:12 +0000

    moved output methods into their own file

--- content_scripts/helpers.js
+++ content_scripts/helpers.js
@@ -40,32 +40,6 @@ function applySvgColorMatrixFilter(matrix, filterName) {
 	addBodyFilter("url", `#${filterId}`);
 }
 
-function showFailures(testName, failureList, failureMessage, successOnEmpty = true) {
-	if(failureList.length) {
-		console.error(failureMessage);
-		console.table(failureList);
-	}
-	
-	if(failureList.length === 0 && successOnEmpty) {
-		showSuccess(testName);
-	}
-}
-
-function showSuccess(testName) {
-	console.info("No problems found");
-}
-
-function showSingleFailure(testName, failureMessage) {
-	console.error(failureMessage);
-}
-
-function showWarnings(testName, warningList, warningMessage) {
-	if(warningList.length) {
-		console.warn(warningMessage);
-		console.table(warningList);
-	}
-}
-
 function getHeadingLevel(heading) {
 	let headingString = heading.tagName.toLowerCase();
 
---
+++ content_scripts/output.js
@@ -0,0 +1,25 @@
+function showFailures(testName, failureList, failureMessage, successOnEmpty = true) {
+	if(failureList.length) {
+		console.error(failureMessage);
+		console.table(failureList);
+	}
+	
+	if(failureList.length === 0 && successOnEmpty) {
+		showSuccess(testName);
+	}
+}
+
+function showSuccess(testName) {
+	console.info("No problems found");
+}
+
+function showSingleFailure(testName, failureMessage) {
+	console.error(failureMessage);
+}
+
+function showWarnings(testName, warningList, warningMessage) {
+	if(warningList.length) {
+		console.warn(warningMessage);
+		console.table(warningList);
+	}
+}
