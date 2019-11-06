249364d74f5de8aa38405e87ee78d4bb9a5009b0
Ashley Sheridan <ash@ashleysheridan.co.uk>
2019-11-06 21:43:49 +0000

    added popup js as ts with basic config

--- popup/choose_test.js
+++ popup/choose_test.js
@@ -1,37 +1,29 @@
 function listenForClicks() {
-	document.addEventListener("click", (e) => {
-		
-		if(e.target.classList.contains("test")) {
-			browser.tabs.query({active: true, currentWindow: true})
-				.then(runTest)
-				.catch(reportError);
-		}
-
-		function runTest(tabs) {
-			let testName = getTestName(e.target);
-			browser.tabs.sendMessage(tabs[0].id, {
-				command: "runTest",
-				testName: testName
-			});
-		}
-
-		function reportError(error) {
-			console.error(`Could not run test: ${error}`);
-		}
-
-		function getTestName(selectedTest) {
-			return selectedTest.dataset.testName;
-		}
-	});
+    document.addEventListener("click", function (e) {
+        if (e.target.classList.contains("test")) {
+            browser.tabs.query({ active: true, currentWindow: true })
+                .then(runTest)["catch"](reportError);
+        }
+        function runTest(tabs) {
+            var testName = getTestName(e.target);
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "runTest",
+                testName: testName
+            });
+        }
+        function reportError(error) {
+            console.error("Could not run test: " + error);
+        }
+        function getTestName(selectedTest) {
+            return selectedTest.dataset.testName;
+        }
+    });
 }
-
-
 function reportExecuteScriptError(error) {
-	document.querySelector("#popup-content").classList.add("hidden");
-	document.querySelector("#error-content").classList.remove("hidden");
-	console.error(`Failed to execute content script: ${error.message}`);
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error("Failed to execute content script: " + error.message);
 }
-
-browser.tabs.executeScript({file: "/content_scripts/tester.js"})
-	.then(listenForClicks)
-	.catch(reportExecuteScriptError);
+browser.tabs.executeScript({ file: "/content_scripts/tester.js" })
+    .then(listenForClicks)["catch"](reportExecuteScriptError);
+//# sourceMappingURL=choose_test.js.map
